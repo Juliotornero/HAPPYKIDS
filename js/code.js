f6c6a6b4cfd8d8128acdcd912cc37fc92aedb659
Julio Tornero <juliotornero8@gmail.com>
2022-05-12 11:54:29 -0500

    Header y Nav terminados (0 styles)

---
+++ js/code.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close-menu');
+const nav = document.querySelector('.navegacion');
+
+function abrir(){
+    menu.classList.add('noshow');
+    closeMenu.classList.remove('noshow');
+    nav.classList.remove('noshow');
+    document.body.style.overflow = 'hidden';
+}
+
+function cerrar(){
+    menu.classList.remove('noshow');
+    closeMenu.classList.add('noshow');
+    nav.classList.add('noshow');
+    document.body.style.overflow = 'scroll';
+}
+
